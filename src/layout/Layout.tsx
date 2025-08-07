6eb5ef577b8c96699e8acfbd9d6e2abbb1ccfd96
Amr <amrabohassab123@gmail.com>
2025-08-08 00:08:39 +0300

    solve warning in layout component container form material ui

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import Container from "@mui/material/Container";
 import Footer from "./Footer";
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Navbar />
-        <Outlet />
+      <Outlet />
       <Footer />
     </div>
   );
